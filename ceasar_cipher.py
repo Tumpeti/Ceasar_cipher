4d919f43af43b741a2cf7e667b36cd2870e5ccbf
Tumpeti <petertumpek20@gmail.com>
2022-12-21 22:43:12 +0100

    decrypt option added

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -5,6 +5,7 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
+"""
 def encrypt(plain_text, shift_amount):
     alphabet_place = []
     for t in plain_text:
@@ -18,7 +19,28 @@ def encrypt(plain_text, shift_amount):
     for position in alphabet_place:
         # print(position)
         encoded_message += alphabet[position]
-    print(encoded_message)
-        
-        
-encrypt(text, shift)
+    print(f"The encoded message is: {encoded_message}")
+
+
+def decrypt(cipher_text, shift_amount):
+    alphabet_place = []
+    for t in cipher_text:
+        # print("1" + t)
+        for letter in alphabet:
+            # print("2" + letter)
+            if letter == t:
+                # print("t=letter")
+                alphabet_place.append(alphabet.index(letter) - shift_amount)
+    decoded_message = ""
+    for position in alphabet_place:
+        # print(position)
+        decoded_message += alphabet[position]
+    print(f"The decoded message is: {decoded_message}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
+
+"""
